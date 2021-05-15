4c5741346f9673b0f295673632ca54bdb548fb86
Laura <laurazenc@gmail.com>
2021-05-15 09:16:28 +0200

    feat: refactor routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Home } from '../pages'
 
 export const Routes: React.FunctionComponent = () => {
   return (
     <Router>
-      <Route exact path="/">
-        hola
-      </Route>
+      <Route exact path="/" component={Home} />
     </Router>
   )
 }
